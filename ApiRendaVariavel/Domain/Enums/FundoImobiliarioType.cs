829dbc06a64fd37f92920593a60cdf2ba2b4402e
iaguuu <iagooliveiraviana@gmail.com>
2024-07-31 12:44:49 -0300

    Domain enumerate

---
+++ ApiRendaVariavel/Domain/Enums/FundoImobiliarioType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace ApiRendaVariavel.Domain.Enums
+{
+    public enum FundoImobiliarioType
+    {
+        [Description("FUNDO IMOBILIARIO")]
+        FII,
+        [Description("FUNDO DE INVESTIMENTO EM INFRAESTRUTURA")]
+        FII_INFRA,
+        [Description("FUNDO DE INVESTIMENTO EM CADEIAS AGROINDÚSTRIAIS")]
+        FII_AGRO,
+        [Description("NAO DEFINIDO")]
+        NAO_DEFINIDO
+    }
+}
