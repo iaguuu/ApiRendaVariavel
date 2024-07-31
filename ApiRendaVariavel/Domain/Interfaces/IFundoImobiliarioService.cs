f66d00f796cadbe330fc5ebe3d45997f7f2569a4
iaguuu <iagooliveiraviana@gmail.com>
2024-07-31 12:53:14 -0300

    Domain Interfaces

---
+++ ApiRendaVariavel/Domain/Interfaces/IFundoImobiliarioService.cs
@@ -0,0 +1,13 @@
+﻿using ApiRendaVariavel.Domain.Entities;
+
+namespace ApiRendaVariavel.Domain.Interfaces
+{
+    public interface IFundoImobiliarioService
+    {
+        List<FundoImobiliario> AllFundosImobiliarios();
+        FundoImobiliario? searchByTicker(string ticker);
+        void Add(FundoImobiliario fundoImobiliario);
+        void Delete(FundoImobiliario fundoImobiliario);
+        void Update(FundoImobiliario fundoImobiliario);
+    }
+}
